52cc3e3368fdc93728f4b18214d3642fcf5cfa6b
Sri Yuniar <sriyuniar541@gmail.com>
2022-10-07 12:54:05 +0900

    tugas fix

--- tugas js1 sri.js
+++ tugas js1 sri.js
@@ -1,8 +1,12 @@
 //task 1
 let nama = "Sri YUniar";
-let adress = "Ambon, Maluku";
+let adress = {
+    kecamatan : "sirimau",
+    kota :"Ambon", 
+    Provinsi : "Maluku"
+}
 let email = "sriyuniar541@gmail.com";
-let isMerried =  false;
+let isMerried = false;
 let hobbies = ["nonton","jalan-jalan","membaca"];
 
 
